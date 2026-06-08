4a604ca108c8b9b92e5721aed3e1dea9f6882086
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2026-06-08 15:21:24 +0300

    fix

--- run_read.sh
+++ run_read.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sbatch srun --account project_2001659 --partition=interactive --time=0:05:00 --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --mem 8000 << BASH
+sbatch --account project_2001659 --partition=interactive --time=0:05:00 --nodes=1 --ntasks-per-node=1 --cpus-per-task=1 --mem 8000 <<BASH
 #!/bin/bash
 module purge
 module load julia
